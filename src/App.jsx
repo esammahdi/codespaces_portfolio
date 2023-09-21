ef105e09c3ed6149d4c0945b00183ba25ccd34ac
Esameldin Mahdi <95491137+esammahdi@users.noreply.github.com>
2023-09-21 06:43:58 +0000

    Changed the name and email in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Esam Mahdi",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "esammahdi111@gmail.com",
+  gitHub: "github.com/esammahdi",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "www.linkedin.com/in/esameldin-bashir",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
